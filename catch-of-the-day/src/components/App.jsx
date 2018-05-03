2015d85144c617ae4637e96e88ffc80aeb4a5d0c
Amandine Latour <amandine.latour+github@gmail.com>
2018-05-03 16:39:43 -0400

    FINIESHED 23: Component Validation with PropTypes

--- catch-of-the-day/src/components/App.jsx
+++ catch-of-the-day/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
@@ -11,6 +12,11 @@ class App extends React.Component {
     fishes: {},
     order: {}
   };
+
+  static propTypes = {
+    match: PropTypes.object
+  };
+
   componentDidMount() {
     // We only want the database of OUR STORE (Our App instance) => SO we fetch the name of the store from App props
     // To create a hierarchy in firebase, we give it the store name, and we nest the fishes inside, adding them after a slash /
